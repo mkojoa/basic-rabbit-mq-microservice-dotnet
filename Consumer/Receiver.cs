332dabad0bce512816cd09d391d38ca2a8c06b61
mkojoa <michael.ameyaw@persol.net>
2020-08-20 23:17:55 +0100

    Basic Project init

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    public class Receiver
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                //declare the queue you want to consume
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                //create a consumer object
+                var consumer = new EventingBasicConsumer(channel);
+
+                //listin to event
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("Received messsage is : {0}", message);
+                };
+
+                channel.BasicConsume("BasicTest", true, consumer);
+
+                Console.WriteLine("Press [Enter] to exist the consumer App");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Producer/Sender.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    public class Sender
+    {
+        public static void Main(string[] args)
+        {
+            //get configuration info
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            
+            //connect to the server.
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel()) //open the channel and send message
+            {
+                //declare a queue
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                //set the message
+                string message = "This is a basic test send from the producer";
+
+                //encode the message before you send
+                var body = Encoding.UTF8.GetBytes(message);
+
+                //publish the message using routing  key
+                channel.BasicPublish("", "BasicTest", null, body);
+
+                Console.WriteLine("Sent message is {0}", message);
+            }
+
+            Console.WriteLine("Press [enter] to exist the sender App");
+            Console.ReadLine();
+        }
+    }
+}
